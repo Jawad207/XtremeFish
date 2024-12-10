9d8360675411dd5e4ceede807288d91cf58d0e2e
Jawadulhassan18 <jawadulhassan18@gmail.com>
2024-12-10 19:08:21 +0500

    changes here

---
+++ server/app.js
@@ -0,0 +1,46 @@
+// server.js
+import express from "express";
+import authentication from "./routes/authentication.js";
+import dashboard from "./routes/dashboard.js"
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cors from "cors";
+
+dotenv.config();
+const port = process.env.PORT || 8080
+
+const server = express();
+server.use(cors());
+const MONGODB_URI = process.env.MONGODB_URI;
+const connectToMongoDB = async () => {
+  try {
+    await mongoose.connect(
+      MONGODB_URI,
+      {}
+    );
+    console.log("Connected to MongoDB");
+  } catch (err) {
+    console.error("Failed to connect to MongoDB", err);
+    // Retry after a delay
+    setTimeout(connectToMongoDB, 5000); // Retry every 5 seconds
+  }
+};
+
+// Start MongoDB connection
+connectToMongoDB();
+
+server.use(express.json());
+server.set("trust proxy", true);
+
+// Example: Custom API route in Express
+server.get("/api/custom", (req, res) => {
+  res.json({ message: "Hello from custom Node.js server!" });
+});
+
+server.use("/auth", authentication);
+server.use("/dashboard", dashboard);
+
+server.listen(port, (err) => {
+  if (err) throw err;
+  console.log("Server running on http://localhost:8080");
+});
